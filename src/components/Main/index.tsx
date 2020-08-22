d2384757e7a626cddcba2cff9bfdc01b600fbd95
Manogel <manoelgomes53@gmail.com>
2020-08-22 15:28:32 -0300

    feat: created main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
-// import * as S from './styles';
+import * as S from './styles';
 
 const Main: React.FC = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Container>
+      <S.Logo src="/img/logo.svg" alt="Logo - React Avançado" />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para tela com um código"
+      />
+    </S.Container>
   );
 };
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,7 +16,9 @@ const GlobalStyles = createGlobalStyle`
     }
 
     body {
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      text-rendering: optimizeLegibility !important;
+      -webkit-font-smoothing: antialiased !important;
     }
 `;
 
