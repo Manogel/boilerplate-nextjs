e5b0c0c9a35a7a613ec5900994812774c8368bb8
Manogel <manoelgomes53@gmail.com>
2020-08-26 15:40:32 -0300

    feat: storybook config

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 import * as S from './styles';
 
-const Main: React.FC = () => {
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS e Styled Components',
+}) => {
   return (
     <S.Container>
       <S.Logo src="/img/logo.svg" alt="Logo - React Avançado" />
-      <S.Title>React Avançado</S.Title>
-      <S.Description>
-        Typescript, ReactJS, NextJS e Styled Components
-      </S.Description>
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
       <S.Illustration
         src="/img/hero-illustration.svg"
         alt="Um desenvolvedor de frente para tela com um código"
