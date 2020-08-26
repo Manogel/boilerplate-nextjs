60262cf7072c5b9c2cef4e2365b2c40ed5e173d7
Manogel <manoelgomes53@gmail.com>
2020-08-26 19:30:49 -0300

    feat: add next images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withPWA = require('next-pwa');
+const withImages = require('next-images')
 const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withImages({
+    esModule: true
+})
 
 module.exports = withPWA({
   pwa: {
